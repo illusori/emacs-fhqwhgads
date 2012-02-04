1cdec36dede1c88023dd3c9f4b2d99f0a7d76506
Sam Graham <git@illusori.co.uk>
2012-02-04 07:42:29 +0000

    Add fhqwhgads-browse and extra packaging stuff like readme and license.

--- fhqwhgads.el
+++ fhqwhgads.el
@@ -1,17 +1,40 @@
 ;;; fhqwhgads.el --- Everybody to the limit.
-
+;;
+;; Copyright (C) 2011-2012  Free Software Foundation, Inc.
+;;
 ;; Author     : Sam Graham <libfhqwhgads-emacs BLAHBLAH illusori.co.uk>
 ;; Maintainer : Sam Graham <libfhqwhgads-emacs BLAHBLAH illusori.co.uk>
 ;; Created    : 3rd February 2012
 ;; Modified   : 3rd February 2012
-;; Version    : 1.0.0
+;; Version    : 1.0.1
 ;; Keywords   : fun homestarrunner strongbad fhqwhgads
-
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;; Please note that the lyrics to Fhqwhgads are not covered by the
+;; above copyright notice and belong to their respective owner.
+;;
 ;;; Commentary:
-
+;;
 ;; A colleague made a joke about Emacs keybindings... I suggested this one.
-
-;;; Code:
+;;
+;;; Usage:
+;; Stick this file in your emacs path and then add to your startup file:
+;;  (require 'fhqwhgads)
+;;  (global-set-key "\C-cfhqwhgads" 'fhqwhgads)
+;;  (global-set-key "\C-cofhqwhgads" 'fhqwhgads-browse)
+;; It's perfectly memorable and obvious, like all Emacs keybindings.
 
 (defgroup fhqwhgads nil
   "Everybody to the limit."
@@ -72,6 +95,11 @@
     (setq fhqwhgads-line 0)
     (setq fhqwhgads-song-timer
       (run-at-time nil fhqwhgads-delay 'fhqwhgads-sing-line))))
+
+(defun fhqwhgads-browse ()
+  "Visit Fhqwhgads in your browser. You'd best use a Flash-capable browser."
+  (interactive)
+  (browse-url fhqwhgads-url))
 
 (provide 'fhqwhgads)
 ;;; fhqwhgads.el ends here
